eb5c8fcf6255f6d0f1267392a5e29460be41eef7
daoge <2318891066@qq.com>
2025-11-05 10:27:26 +0800

    App端保存或修改文章feign接口

--- heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/Impl/ApArticleServiceImpl.java
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/Impl/ApArticleServiceImpl.java
@@ -1,14 +1,24 @@
 package com.heima.article.service.Impl;
 
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.heima.article.mapper.ApArticleConfigMapper;
+import com.heima.article.mapper.ApArticleContentMapper;
 import com.heima.article.mapper.ApArticleMapper;
 import com.heima.article.service.ApArticleService;
 import com.heima.common.constant.ArticleConstants;
+import com.heima.model.article.dtos.ArticleDto;
 import com.heima.model.article.dtos.ArticleHomeDto;
 import com.heima.model.article.pojos.ApArticle;
+import com.heima.model.article.pojos.ApArticleConfig;
+import com.heima.model.article.pojos.ApArticleContent;
 import com.heima.model.common.dtos.ResponseResult;
+import com.heima.model.common.enums.AppHttpCodeEnum;
+import com.sun.org.apache.bcel.internal.generic.NEW;
 import org.apache.commons.lang.StringUtils;
+import org.checkerframework.checker.units.qual.A;
 import org.jsoup.helper.StringUtil;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +29,11 @@ import java.util.List;
 public class ApArticleServiceImpl extends ServiceImpl<ApArticleMapper,ApArticle> implements ApArticleService {
     @Autowired
     private ApArticleMapper apArticleMapper;
+    @Autowired
+    private ApArticleConfigMapper apArticleConfigMapper;
+    @Autowired
+    private ApArticleContentMapper apArticleContentMapper;
+
     private static final short MAX_PAGE_SIZE = 50;
 
     /**
@@ -50,5 +65,47 @@ public class ApArticleServiceImpl extends ServiceImpl<ApArticleMapper,ApArticle>
         List<ApArticle> apArticles = apArticleMapper.loadArticleList(dto, loadType);
         //结果封装和返回
         return ResponseResult.okResult(apArticles);
+    }
+
+    /**
+     * @param dto
+     * @return
+     */
+    @Override
+    public ResponseResult saveArticle(ArticleDto dto) {
+        //检查参数
+        if (dto == null){
+            return ResponseResult.errorResult(AppHttpCodeEnum.PARAM_INVALID);
+        }
+
+        //提取dto的内容
+        ApArticle article = new ApArticle();
+        BeanUtils.copyProperties(dto,article);
+
+        //通过是否存在id判断是增加还是更改操作
+        if(dto.getId() == null){
+            //新增操作
+            //保存文章
+            save(article);
+            //保存文章设定
+            ApArticleConfig apArticleConfig = new ApArticleConfig(article.getId());
+            apArticleConfigMapper.insert(apArticleConfig);
+            //保存文章内容
+            ApArticleContent apArticleContent = new ApArticleContent();
+            apArticleContent.setContent(dto.getContent());
+            apArticleContent.setArticleId(article.getId());
+            apArticleContentMapper.insert(apArticleContent);
+        }else {
+            //有id,修改操作
+            //修改文章
+            updateById(article);
+            //修改文章内容
+            ApArticleContent apArticleContent = apArticleContentMapper.selectOne(Wrappers.<ApArticleContent>lambdaQuery().eq(ApArticleContent::getArticleId, article.getId()));
+            apArticleContent.setContent(dto.getContent());
+            apArticleContentMapper.updateById(apArticleContent);
+        }
+        //返回结果
+        return ResponseResult.okResult(article.getId());
+
     }
 }
